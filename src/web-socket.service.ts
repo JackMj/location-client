e0b832152ad3869c85f9977666f2f569ddd51989
muzi <muzi.jack@gmail.com>
2022-11-21 12:41:02 +0200

    updated client app

--- src/web-socket.service.ts
+++ src/web-socket.service.ts
@@ -18,7 +18,7 @@ export class WebSocketService {
     return new Observable(subscriber => {
       this.socket.on(eventName, (data: any) => {
         subscriber.next(data);
-      })
+      })  
     })
   }
 
